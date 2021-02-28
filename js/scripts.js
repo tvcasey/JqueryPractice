1f9f49d3feace5d63e5d6c72ae350b7d93f86700
Timothy Casey <tvcasey@yahoo.com>
2021-02-28 14:09:14 -0500

    Working with the most simple aspects of jQuery.  It is very logical, but i'd like to do a few tutorials to see its strength as a shorthand for JavaScript.  Also, it is not used in React.

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+
+
+
+$(document).ready(function() {
+    //all jQuery go here:
+            $(".trigger").click(function() {
+            $(".overlay").slideDown();
+    //the button in the box does not work.  See CSS.
+        });
+    });
