7371fda60920be37ce5fc96a6f1d9e0f888fb29d
Timothy Casey <tvcasey@yahoo.com>
2021-03-02 14:30:43 -0500

    Adding to my jQuery knowledge.  This is something I want to build over some time as I now understand how jQuery is structured and how it is useful to html and the web.

--- js/scripts.js
+++ js/scripts.js
@@ -7,4 +7,14 @@ $(document).ready(function() {
             $(".overlay").slideDown();
     //the button in the box does not work.  See CSS.
         });
+        $("button").click(function() {
+            $.get("http://www.devcodecampmusiclibrary.com/api/music", function(data) {
+                $("#result").html(data);
+            //    $("#result").map(data);
+                console.log(data);
+            });
+    });
+        $("button").click(function() {
+            $("p").css({"padding": "10px", "background-color": "yellow"})
+        });
     });
